ab3e46e20361c0d355d771d67fd2dd98626be612
farahAshraf777 <100026250+farahAshraf777@users.noreply.github.com>
2022-12-29 14:51:26 +0200

    API_phase2

---
+++ paymentService/src/main/java/com/Phase2/software2/paymentService/user/User.java
@@ -0,0 +1,27 @@
+package com.Phase2.software2.paymentService.user;
+
+public class User {
+	String userName;
+	String userEmail;
+	String userPassword;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+}
